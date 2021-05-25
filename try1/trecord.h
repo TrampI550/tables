92223d1a4bf74cc4fb53ba006d0d60ac037cc34b
TrampI550 <58778314+TrampI550@users.noreply.github.com>
2021-05-25 14:45:27 +0300

    Update trecord.h

--- try1/trecord.h
+++ try1/trecord.h
@@ -1,22 +1,22 @@
 #pragma once
 #include <iostream>
-typedef std::string TKey;
+typedef std::string TKey;// тип ключа записи
 class TTabRecord 
 {
 
 protected:
-	TKey Key;
-	int* pValue;
+	TKey Key;// ключ
+	int* pValue;// указатель на значение
 public:
 	TTabRecord(TKey k = "", int* pVal = NULL)
 	{
 		Key = k; pValue = pVal;
 	}
 
-	void SetKey(TKey k) {Key = k;}
-	TKey GetKey(void) { return Key; }
-	void SetValuePtr(int* p) { pValue = p; }
-	int* GetValuePtr(void) { return pValue; }
+	void SetKey(TKey k) {Key = k;}// установить значение ключа
+	TKey GetKey(void) { return Key; }// получить значение ключа
+	void SetValuePtr(int* p) { pValue = p; }// установить указатель на данные
+	int* GetValuePtr(void) { return pValue; }// получить указатель на данные
 
 	TTabRecord& operator=(TTabRecord tr)
 	{
@@ -27,7 +27,7 @@ public:
 	virtual int operator< (const TTabRecord &tr) { return Key > tr.Key; }
 	virtual int operator> (const TTabRecord &tr) { return Key < tr.Key; }
 protected:
-	void PrintRec(std::ostream& os) { os << Key << " " << *pValue; }
+	void PrintRec(std::ostream& os) { os << Key << " " << *pValue; }// вывод записи в консоль
 
 friend class TArrayTable;
 friend class TScanTable;
@@ -37,4 +37,4 @@ friend class TTreeTable;
 friend class TArrayhash;
 friend class THashTable;
 };
-typedef TTabRecord* PTTabRecord;
+typedef TTabRecord* PTTabRecord;// указатель на запись
