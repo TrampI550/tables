7bfc61f2d52078f6831d36171cd351df32093cae
TrampI550 <58778314+TrampI550@users.noreply.github.com>
2021-05-25 15:38:38 +0300

    Update treenode.h

--- try1/treenode.h
+++ try1/treenode.h
@@ -6,13 +6,13 @@ typedef TTreeNode* PTTreeNode;
 class TTreeNode : public TTabRecord
 {
 protected:
-	PTTreeNode pLeft, pRight;
+	PTTreeNode pLeft, pRight;// указатели на поддеревья
 public:
 	TTreeNode(TKey k = "", int* pVal = NULL, PTTreeNode pL = NULL, PTTreeNode pR = NULL) :
 		TTabRecord(k, pVal), pLeft(pL), pRight(pR) {}
 
-	PTTreeNode GetLeft(void) const { return pLeft; }
-	PTTreeNode GetRight(void) const { return pRight; }
+	PTTreeNode GetLeft(void) const { return pLeft; }// указатель на левое поддерево
+	PTTreeNode GetRight(void) const { return pRight; }// указатель на правое поддерево
 
 	friend class TTreeTable;
 };
